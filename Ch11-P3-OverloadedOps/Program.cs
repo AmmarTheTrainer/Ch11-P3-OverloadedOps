bd6c8cb54031a230b110ba7f23f5ac1e791932e3
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-10-08 16:41:51 +0500

    commit 2 from head office

--- Ch11-P3-OverloadedOps/Program.cs
+++ Ch11-P3-OverloadedOps/Program.cs
@@ -10,7 +10,23 @@ namespace Ch11_P3_OverloadedOps
     {
         static void Main(string[] args)
         {
+            #region Overlaoding example
 
+            // The + operator with ints.
+            int a = 100;
+            int b = 240;
+            int c = a + b; // c is now 340
+
+            // + operator with strings.
+            string s1 = "Hello";
+            string s2 = " world!";
+            string s3 = s1 + s2; // s3 is now "Hello World!"
+
+            #endregion
+
+
+
+            Console.ReadLine();
         }
     }
 }
